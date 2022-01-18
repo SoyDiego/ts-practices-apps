61faa3cd93920e22ca62f29a1e442731508bb675
soydiego <diegofranchina@gmail.com>
2022-01-18 19:16:00 +0100

    added refresh button

--- thesimpsons-random-quotes/src/App.tsx
+++ thesimpsons-random-quotes/src/App.tsx
@@ -6,14 +6,21 @@ function App() {
 	const [quote, setQuote] = useState<[]>([]);
 
 	useEffect(() => {
+		handleRefresh();
+	}, []);
+
+	const handleRefresh = ():void => {
 		fetch("https://thesimpsonsquoteapi.glitch.me/quotes")
 			.then((res) => res.json())
 			.then((data) => setQuote(data));
-	}, []);
+	};
 
 	return (
 		<div className="tw-h-screen tw-grid tw-place-content-center tw-bg-yellow-500">
-			{quote.length > 0 && quote.map((q: dataApi) => <CardQuote q={q} />)}
+			{quote.length > 0 &&
+				quote.map((q: dataApi) => (
+					<CardQuote handleRefresh={handleRefresh} q={q} />
+				))}
 		</div>
 	);
 }
--- thesimpsons-random-quotes/src/components/CardQuote.tsx
+++ thesimpsons-random-quotes/src/components/CardQuote.tsx
@@ -1,10 +1,12 @@
 import { dataApi } from "../interfaces/quote";
+import IcRefresh from "./Icons/IcRefresh";
 
 type Props = {
 	q: dataApi;
+	handleRefresh: () => void;
 };
 
-const CardQuote = ({q} : Props) => {
+const CardQuote = ({ q, handleRefresh }: Props) => {
 	return (
 		<div className="tw-flex tw-items-center tw-justify-center tw-px-5 tw-py-5">
 			<div
@@ -12,24 +14,29 @@ const CardQuote = ({q} : Props) => {
 				style={{ maxWidth: "500px" }}>
 				<div className="tw-w-full tw-pt-1 tw-pb-5">
 					<div className="tw-overflow-hidden tw-rounded-full tw-w-32 tw-h-32 tw--mt-24 tw-mx-auto">
-						<img src={q.image} alt="" />
+						<img src={q.image} alt={q.character} />
 					</div>
 				</div>
 				<div className="tw-w-full tw-mb-10">
-					<div className="tw-text-3xl tw-text-black tw-text-left tw-leading-tight tw-h-3">
+					<div className="tw-text-4xl tw-text-black tw-text-left tw-leading-tight tw-h-3">
 						“
 					</div>
-					<p className="tw-text-sm tw-text-gray-600 tw-text-center tw-px-5">
+					<p className="tw-text-sm tw-text-gray-500 tw-text-center tw-px-5">
 						{q.quote}
 					</p>
-					<div className="tw-text-3xl tw-text-black tw-text-right tw-leading-tight tw-h-3 tw--mt-3">
+					<div className="tw-text-4xl tw-text-black tw-text-right tw-leading-tight tw-h-3 tw--mt-3">
 						”
 					</div>
 				</div>
-				<div className="tw-w-full">
+				<div className="tw-flex tw-flex-col tw-justify-center tw-items-center">
 					<p className="text-md tw-text-black tw-font-bold tw-text-center">
 						{q.character}
 					</p>
+					<div
+						onClick={handleRefresh}
+						className="tw-mt-4 tw-bg-yellow-500 tw-p-3 tw-rounded-full hover:tw-bg-yellow-400 tw-cursor-pointer tw-shadow-xl">
+						<IcRefresh />
+					</div>
 				</div>
 			</div>
 		</div>
