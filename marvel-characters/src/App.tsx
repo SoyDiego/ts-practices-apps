1f5ba429cda55194297791cd911f378289a9756e
soydiego <diegofranchina@gmail.com>
2022-01-21 22:26:58 +0100

    Api call, env variables, error message and save data in State

--- marvel-characters/src/App.tsx
+++ marvel-characters/src/App.tsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import Form from "./components/Form";
+import { ICharacters } from "./interfaces/ICharacters";
 
 const App = () => {
+	const [inputSearch, setInputSearch] = useState<string>("");
+	const [dataApi, setDataApi] = useState<[]>([]);
+
 	return (
 		<div className="tw-flex tw-justify-center tw-my-12">
-			<Form />
+			<Form
+				inputSearch={inputSearch}
+				setInputSearch={setInputSearch}
+				setDataApi={setDataApi}
+			/>
 		</div>
 	);
 };
--- marvel-characters/src/components/Form.tsx
+++ marvel-characters/src/components/Form.tsx
@@ -1,25 +1,59 @@
-import { FormEvent } from "react";
+import { ChangeEvent, FormEvent } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
-const Form = () => {
-	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+type FormProps = {
+	inputSearch: string;
+	setInputSearch: Function;
+	setDataApi: Function;
+};
+
+const Form = ({ inputSearch, setInputSearch, setDataApi }: FormProps) => {
+	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-		alert("hola");
+		try {
+			const data = await fetch(
+				`http://gateway.marvel.com/v1/public/characters?nameStartsWith=${inputSearch}&ts=1000&${process.env.REACT_APP_MARVEL_PUBLIC_KEY}`
+			);
+
+			const res = await data.json();
+
+
+			if (res.data.count === 0) {
+				toast(`Nothing found. Try again.`, {
+					icon: "😞",
+				});
+				return;
+			}
+
+			setDataApi(res.data.results);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const handleChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
+		setInputSearch(target.value);
 	};
 	return (
-		<form
-			onSubmit={handleSubmit}
-			className="tw-flex tw-flex-col md:tw-flex-row tw-items-center tw-justify-center">
-			<input
-				className="tw-rounded-full tw-p-2 tw-w-72 md:tw-mr-4 tw-mb-4 md:tw-mb-0"
-				type="text"
-				placeholder="Ex: ironman"
-			/>
-			<button
-				className="tw-p-1 tw-rounded-full tw-text-white tw-bg-red-600 hover:tw-bg-red-500"
-				type="submit">
-				Search!
-			</button>
-		</form>
+		<>
+			<form
+				onSubmit={handleSubmit}
+				className="tw-flex tw-flex-col md:tw-flex-row tw-items-center tw-justify-center">
+				<input
+					className="tw-rounded-full tw-p-2 tw-w-72 md:tw-mr-4 tw-mb-4 md:tw-mb-0"
+					type="text"
+					placeholder="Ex: ironman"
+					value={inputSearch}
+					onChange={handleChange}
+				/>
+				<button
+					className="tw-p-1 tw-rounded-full tw-text-white tw-bg-red-600 hover:tw-bg-red-500"
+					type="submit">
+					Search!
+				</button>
+			</form>
+			<Toaster />
+		</>
 	);
 };
 
