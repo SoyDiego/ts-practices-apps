2d4cf2f8a17c8a5334f246fa93331d1456ed020c
soydiego <diegofranchina@gmail.com>
2022-01-22 16:09:32 +0100

    Added layout CharacterInfo

--- marvel-characters/src/components/CharacterInfo.tsx
+++ marvel-characters/src/components/CharacterInfo.tsx
@@ -25,16 +25,49 @@ const CharacterInfo = () => {
 		<div className="tw-container tw-mx-auto tw-my-8">
 			{infoCharacter.map((character) => (
 				<div key={character.id}>
-					<h1 className="tw-text-center">Character Info</h1>
-					<div className="tw-columns-2">
+					<div className="md:tw-columns-2">
 						<div className="tw-w-full tw-flex tw-justify-center tw-rounded-lg tw-p-4">
-							<img className="tw-h-96"
+							<img
+								className="tw-rounded-xl"
 								src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
 								alt={character.name}
 							/>
 						</div>
-						<div className="tw-w-full tw-bg-gray-200 tw-border-2 tw-border-red-400 tw-rounded-lg tw-p-4">
-							<p>{character.description}</p>
+						<div className="tw-w-full tw-rounded-lg tw-p-4">
+							<p className="tw-text-2xl tw-font-extrabold">
+								{character.name}
+							</p>
+							<hr className="tw-my-2 tw-border-2 tw-border-red-500" />
+
+							{character.description !== "" && (
+								<>
+									<p className="tw-text-white tw-text-sm tw-my-4">
+										<span className="tw-font-extrabold">
+											Description:{" "}
+										</span>
+										{character.description}
+									</p>
+									<hr className="tw-my-2 tw-border-2 tw-border-red-500" />
+								</>
+							)}
+							<p className="tw-text-xl tw-font-extrabold">
+								Comics:
+							</p>
+							{character.comics.available !== 0 ? (
+								<ul className="tw-text-xs tw-mt-4 tw-text-white">
+									{character.comics.items.map((comic) => (
+										<li
+											className="tw-mb-1"
+											key={comic.name}>
+											- {comic.name}
+										</li>
+									))}
+								</ul>
+							) : (
+								<p className="tw-text-white tw-text-sm">
+									No comics available! :(
+								</p>
+							)}
 						</div>
 					</div>
 				</div>
