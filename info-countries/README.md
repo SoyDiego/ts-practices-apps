42ed0ce07ecc7d1e9510793d102d35d736597834
soydiego <diegofranchina@gmail.com>
2022-01-24 21:16:03 +0100

    Starting project, installed tailwind, starting UI

---
+++ info-countries/README.md
@@ -0,0 +1,15 @@
+# Info Countries
+---
+### Technologies:
+- TypeScript
+- ReactJS
+- TailwindCSS
+- React Router DOM V6
+---
+### Demo
+
+<!-- ![Screenshot](https://i.imgur.com/ftpMxaF.png)
+
+![Screenshot](https://i.imgur.com/AjX6yub.png) -->
+
+---
---
+++ info-countries/src/App.tsx
@@ -0,0 +1,14 @@
+const App = () => {
+	return (
+		<div className="tw-container tw-mx-auto tw-my-10">
+			<h1 className="tw-text-white/60 tw-font-bold tw-text-xl tw-text-center">
+				Info Countries
+			</h1>
+			<div className="tw-flex tw-justify-center tw-columns-2 tw-border tw-border-gray-500 tw-my-5">
+				<div className="tw-w-2/5 tw-bg-gray-700 tw-h-96">FORM</div>
+				<div className="tw-w-3/5 tw-h-96">FLAGS Countries</div>
+			</div>
+		</div>
+	);
+};
+export default App;
